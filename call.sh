6235a0d2c5dac33187301028d69028f6e039ff2f
zlucifer <dekxter000@gmail.com>
2018-12-13 17:48:01 -0500

    Update call.sh

--- call.sh
+++ call.sh
@@ -1,8 +1,5 @@
-#!/bin/sh
-#hacker indonesia
-
+#!/bin/bash
 echo Selamat datang kak, Siapa nick kaka? #tulisan keluar
-
 read nick #membaca yang ditulis
 echo "================================================="
 echo "== ╦ ╦┌─┐┌─┐┬┌─┌─┐┬─┐  ┬┌┐┌┌┬┐┌─┐┌┐┌┌─┐┌─┐┬┌─┐ =="
@@ -10,22 +7,27 @@ echo "== ╠═╣├─┤│  ├┴┐├┤ ├┬┘  ││││ ││�
 echo "== ╩ ╩┴ ┴└─┘┴ ┴└─┘┴└─  ┴┘└┘─┴┘└─┘┘└┘└─┘└─┘┴┴ ┴ =="
 echo "================================================="
 echo selamat datang $nick ":)"
-echo
-echo "Silahkan masukan nomor telp target"
-echo contoh 0812345678
-read target # masukin no telp
-
-echo
-echo Apakah nomor $target "sudah benar?"
-echo y/n?
-read confirm
-echo
-if [ $confirm = "y" ]; then
-echo Melakukan spam call ke nomor $target
-target_do='http://zpro.000webhostapp.com/api/call.php?nomor='$target
-CURL_RESPONSE=`curl -s -o /dev/null -w "%{http_code}" $target_do`
-#echo $target_do
+get_url=$(curl -s http://zlucifer.com/api/call.php)
+cek='curl -s '$get_url # check status
+response=`curl -s -o /dev/null -w "%{http_code}" $cek`
+if [[ $response != *Kesalahan* ]]; then
+    echo
+    echo "Website Offline/Restart untuk sementara"
 else
-echo Kesalahan, silahkan coba lagi
+    echo
+    echo "Silahkan masukan nomor telp target"
+    echo contoh 0812345678
+    read target # masukin no telp
+    echo
+    echo Apakah nomor $target "sudah benar?"
+    echo y/n?
+    read confirm
+    echo
+    if [ $confirm = "y" ]; then
+        echo Melakukan spam call ke nomor $target
+        cek_target=`curl -s $get_url/call.php?nomor=$target`
+        echo -e $cek_target
+    else
+        echo Kesalahan, silahkan coba lagi
+    fi
 fi
-
